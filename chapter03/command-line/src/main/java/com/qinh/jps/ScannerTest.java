1db4452c02f34b524277f1910384f194ddd5260b
Qh <771600967@qq.com>
2021-08-03 15:19:33 +0800

    feat:添加gc性能调优测试代码和思维导图

---
+++ chapter03/command-line/src/main/java/com/qinh/jps/ScannerTest.java
@@ -0,0 +1,15 @@
+package com.qinh.jps;
+
+import java.util.Scanner;
+
+/**
+ * @author Qh
+ * @version 1.0
+ * @date 2021/8/3 11:10
+ */
+public class ScannerTest {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String info = scanner.next();
+    }
+}
---
+++ chapter03/command-line/src/main/java/com/qinh/jstat/ScannerTest.java
@@ -0,0 +1,17 @@
+package com.qinh.jstat;
+
+import java.util.Scanner;
+
+/**
+ * -Xms100m -Xmx100m
+ *
+ * @author Qh
+ * @version 1.0
+ * @date 2021/8/3 11:09
+ */
+public class ScannerTest {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String info = scanner.next();
+    }
+}
