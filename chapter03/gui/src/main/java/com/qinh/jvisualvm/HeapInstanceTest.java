e2528aa105a09992f4d8ef22b44549f37367d9b4
Qh <771600967@qq.com>
2021-08-05 17:55:38 +0800

    feat:添加jconsole,jvisualvm,mat的使用测试代码和思维导图

---
+++ chapter03/gui/src/main/java/com/qinh/jvisualvm/HeapInstanceTest.java
@@ -0,0 +1,34 @@
+package com.qinh.jvisualvm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * -Xms600m -Xmx600m -XX:SurvivorRatio=8
+ *
+ * @author Qh
+ * @version 1.0
+ * @date 2021/8/5 9:56
+ */
+public class HeapInstanceTest {
+    byte[] buffer = new byte[new Random().nextInt(1024 * 100)];
+    
+    public static void main(String[] args){
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        List<HeapInstanceTest> list = new ArrayList<>();
+        while (true){
+            list.add(new HeapInstanceTest());
+            try {
+                TimeUnit.MILLISECONDS.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ chapter03/gui/src/main/java/com/qinh/jvisualvm/OOMTest.java
@@ -0,0 +1,34 @@
+package com.qinh.jvisualvm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * -Xms600m -Xmx600m -XX:SurvivorRatio=8
+ *
+ * @author Qh
+ * @version 1.0
+ * @date 2021/8/5 10:49
+ */
+public class OOMTest {
+    public static void main(String[] args){
+        List<Picture> list = new ArrayList<>();
+        while (true){
+            try {
+                TimeUnit.MILLISECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            list.add(new Picture(new Random().nextInt(100 * 50)));
+        }
+    }
+}
+
+class Picture{
+    private byte[] pixels;
+    public Picture(int length) {
+        this.pixels = new byte[length];
+    }
+}
