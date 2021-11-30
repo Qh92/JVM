7343a54b36b7dfe78420b29dd8ade69a37f839b4
Qh <771600967@qq.com>
2021-11-30 10:09:28 +0800

    feat:完善文档

--- chapter01/string-table/src/main/java/com/qinh/StringIntern.java
+++ chapter01/string-table/src/main/java/com/qinh/StringIntern.java
@@ -1,6 +1,11 @@
 package com.qinh;
 
 /**
+ * String:intern()是一个本地方法，它的作用是如果字符串常量池中已经包含一个等于此String对象的字符串，
+ * 则返回代表池中这个字符串的String对象的引用；否则，会将此String对象包含的字符串添加到常量池中，并且返回此String对象的引用。
+ * 在JDK 6或更早之前的HotSpot虚拟机中，常量池都是分配在永久代中，我们可以通过-XX:PermSize和-XX:MaxPermSize限制永久代的大小，
+ * 即可间接限制其中常量池的容量。
+ *
  * @author Qh
  * @version 1.0
  * @date 2021/7/9 9:46
