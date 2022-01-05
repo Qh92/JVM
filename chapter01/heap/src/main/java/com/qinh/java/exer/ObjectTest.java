5fefab854dd8f015d2d079c4cfcc04d0e172153a
Qh <771600967@qq.com>
2022-01-05 17:47:23 +0800

    feat:添加boolean/boolean[]占用字节测试代码

--- chapter01/heap/src/main/java/com/qinh/java/exer/ObjectTest.java
+++ chapter01/heap/src/main/java/com/qinh/java/exer/ObjectTest.java
@@ -54,6 +54,20 @@ public class ObjectTest {
         System.out.println("------------");
         Q q = new Q();
         System.out.println(ClassLayout.parseInstance(q).toPrintable());
+
+        System.out.println("===============");
+        /*
+        OFFSET  SIZE      TYPE DESCRIPTION                               VALUE
+          0     4           (object header)                                 01 00 00 00 (00000001 00000000 00000000 00000000) (1)
+          4     4           (object header)(8字节的markword)                 00 00 00 00 (00000000 00000000 00000000 00000000) (0)
+          8     4           (object header)(4字节的Klass Pointer)            05 00 00 f8 (00000101 00000000 00000000 11111000) (-134217723)
+         12     4           (object header)(4字节的数组长度)                  0a 00 00 00 (00001010 00000000 00000000 00000000) (10)
+         16    10   boolean [Z.<elements>(数组数据的长度)                             N/A
+         26     6           (loss due to the next object alignment)
+        Instance size: 32 bytes
+         */
+        boolean[] b = new boolean[10];
+        System.out.println(ClassLayout.parseInstance(b).toPrintable());
     }
 
 
@@ -64,5 +78,5 @@ class Q {
      * boolean[] 占用4个字节
      */
     //boolean b = false;
-    boolean[] b;
+    boolean[] b = new boolean[1];
 }
